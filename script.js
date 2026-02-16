5e62d1be9e4ff4203524102095208ed590c4ce2c
Agapeag7 <agapekyb@gmail.com>
2026-02-16 02:38:45 -0800

    version avec des corrections sauf actualisation du GUI pour saisir une nouvelle reponse

--- script.js
+++ script.js
@@ -807,10 +807,19 @@ class UI {
     }
 
     static updateQuestion(team, operation) {
+        // Lorsqu'une nouvelle question est affichée, réinitialiser l'état de la réponse
         if (team === 1) {
-            if (this.team1Question) this.team1Question.textContent = operation;
+            if (this.team1Question) {
+                if (window.game && game.team1) game.team1.clearAnswer();
+                this.team1Question.innerHTML = `${operation} = <span class="answer" id="team1-answer">?</span>`;
+                this.team1Answer = this.team1Question.querySelector('.answer');
+            }
         } else {
-            if (this.team2Question) this.team2Question.textContent = operation;
+            if (this.team2Question) {
+                if (window.game && game.team2) game.team2.clearAnswer();
+                this.team2Question.innerHTML = `${operation} = <span class="answer" id="team2-answer">?</span>`;
+                this.team2Answer = this.team2Question.querySelector('.answer');
+            }
         }
     }
 
